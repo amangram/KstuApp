980f680f0f2f8ffc3d9955ce6925453ea1f5fb35
Aman <amangram@github.com>
2019-03-23 11:19:34 +0600

    Initial commit

---
+++ app/src/main/java/com/example/kstuapp/MyPagerAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.kstuapp
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                NewsFragment()
+            }
+            else -> {
+                return EventsFragment()
+            }
+        }
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return when (position) {
+            0 -> "News"
+            else -> {
+                return "Events"
+            }
+        }
+    }
+
+}
