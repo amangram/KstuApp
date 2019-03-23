518c469c469e94fc352760d127b523929e815c20
Aman <amangram@github.com>
2019-03-23 12:51:05 +0600

    add recyclerView to NewsFragment

--- app/src/main/java/com/example/kstuapp/NewsFragment.kt
+++ app/src/main/java/com/example/kstuapp/NewsFragment.kt
@@ -3,9 +3,11 @@ package com.example.kstuapp
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_news.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -17,7 +19,22 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  *
  */
+
+data class Movie(val title: String, val year: Int)
+
 class NewsFragment : Fragment() {
+
+
+    private val mNicolasCageMovies = listOf(
+        Movie("Raising Arizona", 1987),
+        Movie("Vampire's Kiss", 1988),
+        Movie("Con Air", 1997),
+        Movie("Gone in 60 Seconds", 1997),
+        Movie("National Treasure", 2004),
+        Movie("The Wicker Man", 2006),
+        Movie("Ghost Rider", 2007),
+        Movie("Knowing", 2009)
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +42,23 @@ class NewsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // RecyclerView node initialized here
+        list_recycler_view.apply {
+            // set a LinearLayoutManager to handle Android
+            // RecyclerView behavior
+            layoutManager = LinearLayoutManager(activity)
+            // set the custom adapter to the RecyclerView
+            adapter = ListAdapter(mNicolasCageMovies)
+        }
+    }
+
+    companion object {
+        fun newInstance(): NewsFragment = NewsFragment()
+
     }
 
 
